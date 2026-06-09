4d1ba595d9ba24f0c17602a2efc1c385e0190dc3
Rohan Deshpande <65016586+Rohan-Repo@users.noreply.github.com>
2026-06-08 20:16:07 -0400

    Updating CREATE Table Statements for SQLite

--- DB-Wise-Codebase/SQLite/SQLite-Math-String-DateTime-Functions/SQLite-DateTime-Functions.sql
+++ DB-Wise-Codebase/SQLite/SQLite-Math-String-DateTime-Functions/SQLite-DateTime-Functions.sql
@@ -36,26 +36,18 @@ SELECT strftime( '%W', 'now' ) as currWeek, strftime( '%W', 'now', '+21 days' ) 
 SELECT strftime('%Y-%m-%d', 'now', '+14 days'); 
 
 -- Table Data 
-CREATE TABLE Student( sID INTEGER PRIMARY KEY AUTOINCREMENT, sBDate DATETIME, sName TEXT );
+CREATE TABLE Experts( eID INTEGER PRIMARY KEY AUTOINCREMENT, eBDate DATETIME, eName TEXT, eTech TEXT );
 
-INSERT INTO Student (sBDate, sName) VALUES ('1961-04-09 01:09:11', 'Dwayne Richard Hipp' );
-INSERT INTO Student (sBDate, sName) VALUES ('1956-01-31 10:10:10', 'Guido Van Rossum');
-INSERT INTO Student (sBDate, sName) VALUES ('1948-09-10 18:40:10', 'Charles Simonyi');
-INSERT INTO Student (sBDate, sName) VALUES ('1791-12-26 16:16:16', 'Charles Babbage');
-INSERT INTO Student (sBDate, sName) VALUES ('2020-01-01 12:12:12', 'John Doe' );
-INSERT INTO Student (sBDate, sName) VALUES ('2021-03-03 13:13:13', 'Jane Doe' );
-INSERT INTO Student (sBDate, sName) VALUES ('1990-06-15 10:30:00', 'Harvey Specter');
-INSERT INTO Student (sBDate, sName) VALUES ('1991-09-21 14:45:00', 'Mike Ross');
-INSERT INTO Student (sBDate, sName) VALUES ('1992-03-05 09:15:00', 'Rachel Zane');
-INSERT INTO Student (sBDate, sName) VALUES ('1990-01-19 13:20:00', 'Louis Litt');
-INSERT INTO Student (sBDate, sName) VALUES ('1993-11-30 08:40:00', 'Donna Paulsen');
-INSERT INTO Student (sBDate, sName) VALUES ('1994-07-25 16:05:00', 'Jessica Pearson');
-INSERT INTO Student (sBDate, sName) VALUES ('1995-02-14 11:10:00', 'Katrina Bennett');
-INSERT INTO Student (sBDate, sName) VALUES ('1996-05-08 07:50:00', 'Samantha Wheeler');
-INSERT INTO Student (sBDate, sName) VALUES ('1992-10-03 12:30:00', 'Sheila Sazs');
-INSERT INTO Student (sBDate, sName) VALUES ('1991-12-22 15:00:00', 'Harold Gunderson');
+INSERT INTO Experts (eBDate, eName, eTech)
+VALUES
+('1956-01-31 10:30:00', 'Guido van Rossum', 'Python'),
+('1961-04-09 01:09:11', 'Dr. Richard Hipp', 'SQLite'),
+('1948-09-10 08:40:00', 'Charles Simonyi', 'Microsoft Word & Excel'),
+('1791-12-26 16:05:00', 'Charles Babbage', 'Analytical Engine'),
+('1941-09-09 12:00:00', 'Dennis Ritchie', 'C'),
+('1955-06-08 12:00:00', 'Sir Tim Berners-Lee', 'World Wide Web');
 
-SELECT * FROM Student;
+SELECT * FROM Experts;
 
 -- Display DateTime with strftime
-SELECT sName, strftime('%d-%m %H:%M', sBDate) AS DMHM, strftime('%d-%m-%Y %H:%M:%S %p', sBDate) as ISTDateTime FROM Student;
+SELECT eName, strftime('%d-%m %H:%M', eBDate) AS DMHM, strftime('%d-%m-%Y %H:%M:%S %p', eBDate) as ISTDateTime FROM Experts;
--- DB-Wise-Codebase/SQLite/SQLite-Triggers-WindowFunctions-CTEs-Transactions/01-SQLite-CREATE-Employee-Table.sql
+++ DB-Wise-Codebase/SQLite/SQLite-Triggers-WindowFunctions-CTEs-Transactions/01-SQLite-CREATE-Employee-Table.sql
@@ -1,24 +1,18 @@
 -- Clear Table Contents:
 DROP TABLE IF EXISTS Employee;
 
-CREATE TABLE Employee( empId INT PRIMARY KEY, empName TEXT, empDept TEXT, empSalary REAL, empCity TEXT, empCountry TEXT );
+CREATE TABLE Employee( empId INTEGER PRIMARY KEY AUTOINCREMENT, empName TEXT, empDept TEXT, empSalary REAL, empCity TEXT, empCountry TEXT );
 
-INSERT INTO Employee VALUES ( 1, 'Harvey Specter', 'Legal', 100000, 'New York', 'USA' ); 
-
-INSERT INTO Employee VALUES ( 2, 'Jessica Pearson', 'CEO', 200000, 'Seattle', 'USA' ); 
-
-INSERT INTO Employee VALUES ( 3, 'Mike Ross', 'Research', 50000, 'Toronto', 'Canada' ); 
-
-INSERT INTO Employee VALUES ( 4, 'Louis Litt', 'COO', 150000, 'London', 'UK' ); 
-
-INSERT INTO Employee VALUES (5, 'Rachel Zane', 'Legal', 80000, 'New York', 'USA');
-
-INSERT INTO Employee VALUES (6, 'Donna Paulsen', 'Legal', 90000, 'New York', 'USA');
-
-INSERT INTO Employee VALUES (7, 'Rober Zane', 'Legal', 125000, 'Seattle', 'USA');
-
-INSERT INTO Employee VALUES (8, 'Harold Gunderson', 'Research', 35000, 'Toronto', 'Canada');
-
-INSERT INTO Employee VALUES (9, 'Benjamin', 'Research', 45000, 'Toronto', 'Canada');
+INSERT INTO Employee (empName, empDept, empSalary, empCity, empCountry)
+VALUES
+('Harvey Specter', 'Legal', 100000, 'New York', 'USA'),
+('Jessica Pearson', 'CEO', 200000, 'Seattle', 'USA'),
+('Mike Ross', 'Research', 50000, 'Toronto', 'Canada'),
+('Louis Litt', 'COO', 150000, 'London', 'UK'),
+('Rachel Zane', 'Legal', 80000, 'New York', 'USA'),
+('Donna Paulsen', 'Legal', 90000, 'New York', 'USA'),
+('Robert Zane', 'Legal', 125000, 'Seattle', 'USA'),
+('Harold Gunderson', 'Research', 35000, 'Toronto', 'Canada'),
+('Benjamin', 'Research', 45000, 'Toronto', 'Canada');
 
 SELECT * FROM Employee ORDER BY empSalary DESC;
